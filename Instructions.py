d52905b73145c1ec4be3aeefd654f46ce9a88e6c
Daniel Cave <81333059+19010-HFHS@users.noreply.github.com>
2022-11-11 00:12:37 +0000

    added version 1 of instructions function

---
+++ Instructions.py
@@ -0,0 +1,15 @@
+#  Instructions for Recipe Cost
+#  By Eden Cave
+#  11/11/2022
+#  Version 1
+
+#  defines instructions given
+instructions = "This program is called the Recipe Cost Calculator. It has been developed to help you manage your finances when cooking a recipe. To begin, you must enter the name of your recipe and the size of 1 serving, follwed by the ingredients you need, how much you need, and the prices and amounts they are sold for. The RC Calculator will then calculate the total price of the recipe, along with the price per serving."
+#  Asks the user if they have used the program before
+tutorial = input("Have you used this program before?")
+#  skips instructions if the user has used program before
+if tutorial.lower == "yes" or tutorial.lower == "y":
+  print("please enter your recipe's name and the serving size")
+#  gives instructions if the user has not used program before
+elif tutorial.lower == "no" or tutorial.lower == "n":
+  print(instructions)
