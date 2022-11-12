4635e85586ad025ba4a8a93bfa92013f1ce99eac
Daniel Cave <81333059+19010-HFHS@users.noreply.github.com>
2022-11-12 22:15:35 +0000

    updated instructions message

--- Instructions_Iteration_2.py
+++ Instructions_Iteration_2.py
@@ -7,7 +7,7 @@
 recipe_name = []
 serving_size = []
 #  defines instructions given
-instructions = "This program is called the Recipe Cost Calculator. It has been developed to help you manage your finances when cooking a recipe. To begin, you must enter the name of your recipe and the size of 1 serving, follwed by the ingredients you need, how much you need, and the prices and amounts they are sold for. The RC Calculator will then calculate the total price of the recipe, along with the price per serving."
+instructions = "This program is called the Recipe Cost Calculator. It has been developed to help you manage your finances when cooking a recipe. To begin, you must enter the name of your recipe and the size of 1 serving, follwed by the ingredients you need, how much you need, and the prices and amounts they are sold for (enter xxx when you are done entering the required values). The RC Calculator will then calculate the total price of the recipe, along with the price per serving."
 #  Asks the user if they have used the program before
 tutorial = input("Have you used this program before? ")
 #  skips instructions if the user has used program before
