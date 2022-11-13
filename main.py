4443dd4a35a3389241c3c6d5d1305683a6943b43
Daniel Cave <81333059+19010-HFHS@users.noreply.github.com>
2022-11-13 22:13:37 +0000

    tested, fixed and improved Ingredients List

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #  Ingredients List for recipe cost
 #  By Eden Cave
 #  13/11/2022
-#  Version 6
+#  Version 11
 
 #  set up for ingredients list and exit code
 exit_code = False
@@ -10,24 +10,28 @@ ingredients = []
 #  asks for user input
 while exit_code == False:
   #  asks for name of ingredient, stores answer and checks for exit code
-  ingredient_name = input("Please enter and ingredient: ")
-  #  exits program if exit code is detected
-  if ingredient_name == "xxx":
-    exit_code = True
+  try:
+    ingredient_name = input("Please enter an ingredient: ")
+    #  exits program if exit code is detected
+    if ingredient_name == "xxx":
+      exit_code = True
+      break
   #  checks that ingredient name isn't a number
-  if type(ingredient_name) != str:
+  except:
     print("Error: please enter a valid string")
     break
   else:
     ingredients.append(ingredient_name)
   
   #  asks for amount needed, stores answer and checks for exit code
-  amount_needed = input("Please enter the amount needed (in grams): ")
-  #  exits program if exit code is detected
-  if amount_needed == "xxx":
-    exit_code = True
+  try:
+    amount_needed = int(input("Please enter the amount needed (in grams): "))
+    #  exits program if exit code is detected
+    if amount_needed == "xxx":
+      exit_code = True
+      break
   #  checks that amount needed is a whole number
-  if type(amount_needed) != int:
+  except:
     print("Error: please enter a valid number")
     break
   #  prevents invalid inputs from being added to list
@@ -35,24 +39,28 @@ while exit_code == False:
     ingredients.append(amount_needed)
 
   #  Asks for price, stores answer and checks for exit code
-  price = input("please enter the price: $")
-  #  exits program if exit code is detected
-  if price == "xxx":
-    exit_code = True
+  try:
+    price = float(input("please enter the price: $"))
+    #  exits program if exit code is detected
+    if price == "xxx":
+      exit_code = True
+      break
   #  checks that the price is a float (decimal number)
-  if type(price) != float:
+  except:
     print("Error: please enter a valid number")
     break
   else:
     ingredients.append(price)
 
   # asks for amount sold, stores answer and checks for exit code
-  amount_sold = input("please enter the amount sold (in grams): ")
-  #  exits program if exit code is detected
-  if amount_sold == "xxx":
-    exit_code = True
+  try:
+    amount_sold = int(input("please enter the amount sold (in grams): "))
+    #  exits program if exit code is detected
+    if amount_sold == "xxx":
+      exit_code = True
+      break
   #  checks that the amount sold is a whole number
-  if type(amount_sold) != int:
+  except:
     print("Error: please enter a valid number")
     break
   else:
