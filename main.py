f724e63dae88c743b54f069735eb61a5be710d24
Daniel Cave <81333059+19010-HFHS@users.noreply.github.com>
2022-11-16 01:14:49 +0000

    updated Amount and Cost

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#  Amount and Cost for Recipe Cost
+#  Amount and Cost for Recipe Cost (Iteration 2)
 #  By Eden Cave
-#  15/11/2022
-#  version 2
+#  16/11/2022
+#  version 1
 
 #  stores values for later
 amount_required = ""
@@ -13,11 +13,23 @@ result = []
 #  asks for user input
 while exit_code != True:
   #  asks user for amount required
-  amount_required = int(input("Please enter an amount required: "))
-  #  asks user for cost
-  cost = float(input("Please enter a cost: $"))
-  #  asks user for amount the ingredient is sold as
-  amount_sold_as = int(input("Please enter amount the ingredient is sold as: "))
+  try:
+    amount_required = int(input("Please enter an amount required: "))
+  except:
+    print("Error: please enter a whole number")
+    break
+    #  asks user for cost
+  try:  
+    cost = float(input("Please enter a cost: $"))
+  except:
+    print("Error: please enter a valid decimal")
+    break
+    #  asks user for amount the ingredient is sold as
+  try:    
+    amount_sold_as = int(input("Please enter amount the ingredient is sold as: "))
+  except:
+    print("Error: please enter a whole number")
+    break
   #  does the mathmatical calculations
   base_price = (cost / amount_sold_as)
   result = (base_price * amount_required)
